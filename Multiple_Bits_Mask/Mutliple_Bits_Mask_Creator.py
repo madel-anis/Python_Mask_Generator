aa668f971b5de49cf99d39edaa8ffd6a6e26c4aa
madel-anis <madel.anis11@gmail.com>
2020-05-15 21:06:30 +0200

    Signed-off-by: madel-anis <madel.anis11@gmail.com> Mask Generator Version 1.0

---
+++ Multiple_Bits_Mask/Mutliple_Bits_Mask_Creator.py
@@ -0,0 +1,26 @@
+print("Hello Player, Welcome to the Multiple Bits Mask Creator Program\n")
+
+REG_NAME                = input("\nPlease Enter your REGISTER Name: ")
+MASK_NAME               = input("\nPlease Enter your MASK Name: ")
+NO_OF_BITS_str          = input("\nPlease Enter your number of Bits: ")
+
+print("\n")
+
+NO_OF_BITS          = int(NO_OF_BITS_str)
+
+Mask = 0
+
+for i in range(1,NO_OF_BITS+1):
+  Bits_Pos     = input("Please Enter your " + str(i) + " Bit position: ")
+  Bits_Pos_int = int(Bits_Pos)
+  Mask         = Mask | (1 << Bits_Pos_int)
+
+#Creating File of the Masks
+file = open( str(REG_NAME) + "_Defines.h",'w')
+
+
+file.write("#define " + REG_NAME + "_" + MASK_NAME  + "_MASK_" + "\t\t (0x"+  format( (Mask) , '08X' )                     +") \n")
+file.write("#define " + REG_NAME + "_" + MASK_NAME  + "_CLEAR" + "\t\t (0x"+  format( (~(Mask) + 1 + 0xFFFFFFFF) , '08X' ) +") \n")
+
+
+file.close()
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Python_Mask_Generator
 Python Scripts to Generate #defines Masks of the Registers bits of any Register in any Micro-Controller
+
+One Bit MASK Generator:
+
+Give you the #define of the setting mask and clearing mask of single bits with different name of a whole Register
+
+<p align="center">
+  <img width="460" height="300" src="Images/Register.PNG">
+</p>
+
+The steps to use the 
+ 1.Enter the name of the Register
+ 2.Enter the names of the required Bits needed up to 32 bits
+
+a header file will be generated with #defines. 
+
+<p align="center">
+  <img width="460" height="300" src="Images/Definitions.PNG">
+</p>
